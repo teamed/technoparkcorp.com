aa94c6835f5762526c77c4d509b08065421491b9
Yegor Bugayenko <yegor@tpc2.com>
2009-07-12 06:22:02 +0000

    cached calculation in Navigation.php

--- php/src/application/Model/Navigation.php
+++ php/src/application/Model/Navigation.php
@@ -26,6 +26,13 @@
 class Model_Navigation {
 
     /**
+     * Cache of navigation map
+     *
+     * @var Zend_Cache
+     */
+    protected static $_cache;
+    
+    /**
      * XML text for the article, if given
      *
      * @var XMLDocument
@@ -40,7 +47,25 @@ class Model_Navigation {
      * @return void
      */
     public static function populateNavigation(&$container, $activePage) {
-        self::_addMenuPages($container, $activePage);
+
+        if (self::_cache()->test('map')) {
+        
+            // we already have this set of pages in cache
+            $container->setPages(self::_cache()->load('map'));
+    
+        } else {
+
+            // we should calculate them again
+            self::_addMenuPages($container);
+
+            // and save to cache
+            self::_cache()->save($container->getPages(), 'map');
+
+        }
+
+        // mark active page as "active"
+        self::_markActivePage($container, $activePage);
+
     }
 
     /**
@@ -53,7 +78,7 @@ class Model_Navigation {
      * @param string File path with XML files
      * @return void
      */
-    protected static function _addMenuPages($container, $activePage, $path = CONTENT_PATH, $prefix = '') {
+    protected static function _addMenuPages($container, $path = CONTENT_PATH, $prefix = '') {
 
         // get full list of XML files in content directory
         foreach (glob($path . '/*.xml') as $file) {
@@ -71,13 +96,10 @@ class Model_Navigation {
             $page = new Zend_Navigation_Page_Uri(array(
                 'label' => $article->label,
                 'title' => $article->title,
+                'path' => $fullLabel,
                 'uri' => Zend_Registry::getInstance()->view->staticUrl($fullLabel),
                 'class' => 'l' . substr_count($fullLabel, '/'),
             ));
-
-            // mark active menu item
-            if ($fullLabel == $activePage)
-                $page->active = true;
 
             // hide unnecessary menu items
             if (!$article->visible)
@@ -89,10 +111,62 @@ class Model_Navigation {
             // if it was a directory - add sub files to this page
             // as a sub menu
             if (is_dir($path . '/' . $label))
-                self::_addMenuPages($page, $activePage, $path . '/' . $label, $fullLabel);
+                self::_addMenuPages($page, $path . '/' . $label, $fullLabel);
         }
 
     }
 
+    /**
+     * Find and mark active page as "active"
+     *
+     * @param Zend_Navigation
+     * @param string Page path
+     * @return void
+     */
+    protected static function _markActivePage($container, $activePage) {
+
+        // page label "about/facts" will be converted to ("about", "facts")
+        $sections = explode('/', $activePage);
+
+        for ($i=0; $i<count($sections); $i++) {
+            $page = $container->findOneBy('path', implode('/', array_slice($sections, 0, $i+1)));
+            if (is_null($page))
+                continue;
+
+            // set it as active
+            $page->active = true;
+        }
+
+    }
+
+    /**
+     * Get an instance of cache, to save the parsed Navigation map
+     *
+     * @return Zend_Cache
+     */
+    protected static function _cache() {
+
+        if (self::$_cache != false)
+            return self::$_cache;
+
+        self::$_cache = Zend_Cache::factory('Core', 'File', array(
+            'caching' => true,
+            'cache_id_prefix' => 'panel2nav' . FaZend_Revision::get(),
+            'lifetime' => null, // live forever
+            'automatic_serialization' => true,
+            'automatic_cleaning_factor' => false,
+            'write_control' => true,
+            'logging' => false,
+            'ignore_user_abort' => true), array(
+
+            'cache_dir' => sys_get_temp_dir(),
+            'hashed_directory_level' => 0,
+            'read_control' => true,
+            'file_locking' => true,
+            'file_name_prefix' => 'panel2navigation'));
+
+        return self::$_cache;
+
+    }
 
 }
