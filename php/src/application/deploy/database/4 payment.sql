ad1af549d9c24684b8e225910962513f6713bde0
Yegor Bugayenko <yegor@tpc2.com>
2009-10-29 12:05:12 +0000

    payments SQL table intro

---
+++ php/src/application/deploy/database/4 payment.sql
@@ -0,0 +1,21 @@
+---
+--- Payments 
+---
+
+CREATE TABLE IF NOT EXISTS `payment` (
+    
+    `id` INT NOT NULL AUTO_INCREMENT COMMENT 'Unique ID of the payment',
+    `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'When it was created',    
+    `user` VARCHAR(120) COMMENT 'Email of the user related to this payment',
+    `context` VARCHAR(50) COMMENT 'Project name, for example',
+    
+    `rate` VARCHAR(15) COMMENT 'Active user rate at the moment of payment',
+    `original` VARCHAR(20) COMMENT 'Payment amount with currency',
+    `amount` INT NOT NULL COMMENT 'Payment amount in USD cents',
+
+    `details` MEDIUMTEXT NOT NULL COMMENT 'Details of the payment made',
+    
+    PRIMARY KEY (`id`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
