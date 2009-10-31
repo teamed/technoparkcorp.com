5d6ac3729e71fce40baf1f4cb7619ff5d178f968
Yegor Bugayenko <yegor@tpc2.com>
2009-10-31 21:48:51 +0000

    db tables modified for supplier

--- php/src/application/deploy/database/6 ability.sql
+++ php/src/application/deploy/database/6 ability.sql
@@ -1,18 +1,18 @@
 ---
---- Ability of a Performer to take participation in a project
+--- Ability of a Supplier to take participation in a project
 ---
 
 CREATE TABLE IF NOT EXISTS `ability` (
     
     `id` INT NOT NULL AUTO_INCREMENT COMMENT 'Unique ID of this ability',
-    `performer` INT NOT NULL COMMENT 'Id of the performer',
-    `role` VARCHAR(150) NOT NULL COMMENT 'Text name of role which could be filled by the performer',
+    `supplier` INT NOT NULL COMMENT 'Id of the supplier',
+    `role` VARCHAR(150) NOT NULL COMMENT 'Text name of role which could be filled by the supplier',
     `price` VARCHAR(10) NOT NULL COMMENT 'Price per hour in this role',
 
     PRIMARY KEY (`id`),
     UNIQUE(`performer`, `role`),
 
-    CONSTRAINT `fk_ability_performer` FOREIGN KEY (`performer`) REFERENCES `performer` (`id`) ON UPDATE CASCADE
+    CONSTRAINT `fk_ability_supplier` FOREIGN KEY (`supplier`) REFERENCES `supplier` (`id`) ON UPDATE CASCADE
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
