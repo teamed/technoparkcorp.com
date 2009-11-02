ee68821f06ad3801ceafdc0fe5f72cdf61b838af
Yegor Bugayenko <yegor@tpc2.com>
2009-11-02 15:17:09 +0000

    payment is linked to supplier by FK

--- php/src/application/deploy/database/10 payment.sql
+++ php/src/application/deploy/database/10 payment.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS `payment` (
     
     `id` INT NOT NULL AUTO_INCREMENT COMMENT 'Unique ID of the payment',
     `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'When it was created',    
-    `user` VARCHAR(120) COMMENT 'Email of the user related to this payment',
+    `supplier` INT COMMENT 'ID of the related supplier, if any',
     `context` VARCHAR(50) COMMENT 'Project name, for example',
     
     `rate` VARCHAR(15) COMMENT 'Active user rate at the moment of payment',
@@ -18,7 +18,8 @@ CREATE TABLE IF NOT EXISTS `payment` (
     `details` MEDIUMTEXT NOT NULL COMMENT 'Details of the payment made',
     
     PRIMARY KEY (`id`),
-    UNIQUE(`reason`)
+    UNIQUE(`supplier`, `context`, `reason`),
+    CONSTRAINT `fk_payment_supplier` FOREIGN KEY (`supplier`) REFERENCES `supplier` (`id`) ON UPDATE CASCADE
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
