607ca12195a407c97e16d38d2fcccdb945c0a1e5
Yegor Bugayenko <yegor@tpc2.com>
2009-12-21 15:13:38 +0000

    kill payments if there are many of them

--- php/test/Mocks/artifacts/Statements/Statement.php
+++ php/test/Mocks/artifacts/Statements/Statement.php
@@ -29,8 +29,15 @@ class Mocks_theStatement
     public static function get($supplier = 'test@example.com') 
     {
         require_once 'Mocks/artifacts/Statements/Statement/Payment.php';
+
+        // kill payments, if they are too many of them
+        if (count(thePayment::retrieve()->fetchAll()) > 50) {
+            thePayment::retrieve()->table()->getAdapter()->query('DELETE FROM payment');
+        }
+            
         for ($i = 0; $i < 5; $i++)
             Mocks_thePayment::make($supplier, (rand(1,9) > 5 ? '' : '-') . rand(100, 200) . ' EUR');
+            
         return Model_Artifact::root()->statements[$supplier];
     }
     
