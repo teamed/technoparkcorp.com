db0904fb5a19f12d2ba111524882d16295bd54fc
Yegor Bugayenko <yegor@tpc2.com>
2009-12-25 18:31:39 +0000

    more log

--- php/src/application/Model/Wobot.php
+++ php/src/application/Model/Wobot.php
@@ -52,6 +52,8 @@ abstract class Model_Wobot implements Model_Wobot_Interface
     {
         self::$_wobots = new ArrayIterator();
         
+        logg("Projects in registry: " . implode(', ', 
+            array_keys(iterator_to_array(FaZend_Pos_Abstract::root()->projectRegistry))));
         // list all wobot names
         foreach (self::_getAllNames() as $name) {
             $wobotClass = __CLASS__ . '_' . $name;
