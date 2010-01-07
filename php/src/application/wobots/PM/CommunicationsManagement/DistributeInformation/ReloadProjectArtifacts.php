69deaebb3545a3ef9c8f07c20b2d076670b3e9a1
Yegor Bugayenko <yegor@tpc2.com>
2010-01-07 09:32:44 +0000

    better logging in artifacts reloading decision

--- php/src/application/wobots/PM/CommunicationsManagement/DistributeInformation/ReloadProjectArtifacts.php
+++ php/src/application/wobots/PM/CommunicationsManagement/DistributeInformation/ReloadProjectArtifacts.php
@@ -47,6 +47,7 @@ class ReloadProjectArtifacts extends Model_Decision_PM
             if (!($this->_project->$property instanceof Model_Artifact_Passive))
                 continue;
                 
+            logg("Reloading of $property...");
             $this->_project->$property->reload();
             logg("Artifact reloaded: $property");
             $reloaded[] = $property;
