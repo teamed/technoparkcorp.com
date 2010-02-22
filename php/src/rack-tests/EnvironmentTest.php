c13349b8d0af3d76c26ae552da079f8678c80a6a
Yegor Bugayenko <yegor@tpc2.com>
2010-02-22 16:36:17 +0000

    validate extensions

--- php/src/rack-tests/EnvironmentTest.php
+++ php/src/rack-tests/EnvironmentTest.php
@@ -5,7 +5,17 @@ class EnvironmentTest extends PhpRack_Test
 
     public function testSvnExists()
     {
+    }
+
+    public function testPhpIsConfigured()
+    {
         $this->assert->php->version->atLeast('5.2');
+        $this->assert->php->extensions
+            ->isLoaded('finfo')
+            ->isLoaded('simplexml')
+            ->isLoaded('xsl');
+
+        $this->assert->php->extensions->finfo->isAlive();
     }
 
 }
