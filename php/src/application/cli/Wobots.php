5be78be402222a72fe7e6014d39b65553e78515e
Yegor Bugayenko <yegor@tpc2.com>
2010-03-24 09:42:11 +0000

    concurrency flag in wobots introduced

--- php/src/application/cli/Wobots.php
+++ php/src/application/cli/Wobots.php
@@ -29,12 +29,24 @@ class Wobots extends FaZend_Cli_Abstract
 {
 
     /**
+     * Flag name, file absolute name
+     *
+     * @var string
+     */
+    protected $_flag = TEMP_PATH . '/wbts-busy-flag.txt';
+
+    /**
      * Executor of a command-line command
      *
      * @return int Exit code
      */
     public function execute()
     {
+        // if we're working with another wobot at the moment
+        if ($this->_serverIsBusyNow()) {
+            return self::RETURNCODE_OK;
+        }
+        
         foreach (Model_Wobot::retrieveAll() as $wobot) {
             if (!$wobot->isInOfficeNow()) {
                 continue;
@@ -42,7 +54,59 @@ class Wobots extends FaZend_Cli_Abstract
             Model_User::logIn($wobot->getEmail());
             $wobot->execute();
         }
+        
+        // release server, the task is done
+        $this->_releaseServer();
+        
         return self::RETURNCODE_OK;
+    }
+    
+    /**
+     * Server is busy at the moment?
+     *
+     * If server is not busy, this method sets a busy flag for it,
+     * and returns false.
+     *
+     * @return boolean
+     * @see execute()
+     * @throws Wobots_FlagCreationFailedException
+     */
+    protected function _serverIsBusyNow() 
+    {
+        if (@file_exists($this->_flag)) {
+            return true;
+        }
+        if (@file_put_contents($this->_flag, getmypid()) === false) {
+            FaZend_Exception::raise(
+                'Wobots_FlagCreationFailedException',
+                "Failed to create flag: '{$this->_flag}'"
+            );
+        }
+        return false;
+    }
+
+    /**
+     * Release server and allow other wobots to work
+     *
+     * @return void
+     * @see execute()
+     * @throws Wobots_FlagAbsentException
+     * @throws Wobots_FlagUnlinkException
+     */
+    protected function _releaseServer() 
+    {
+        if (!@file_exists($this->_flag)) {
+            FaZend_Exception::raise(
+                'Wobots_FlagAbsentException',
+                "Flag not found: '{$this->_flag}'"
+            );
+        }
+        if (@unlink($this->_flag) === false) {
+            FaZend_Exception::raise(
+                'Wobots_FlagUnlinkException',
+                "Failed to delete flag: '{$this->_flag}'"
+            );
+        }
     }
 
 }
