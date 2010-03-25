ebda9decdaad5c1a490d4b398f505c0871a7d838
Yegor Bugayenko <yegor@tpc2.com>
2010-03-25 07:17:18 +0000

    log test refactored

--- php/src/rack-tests/LogTest.php
+++ php/src/rack-tests/LogTest.php
@@ -6,18 +6,12 @@
 class LogTest extends PhpRack_Test
 {
 
-    const FILENAME = '/home/beta/log/php_errors.log';
-
-    public function testFileExists()
-    {
-        if (!file_exists(self::FILENAME)) {
-            $this->fail('Log file ' . self::FILENAME . ' is not found');
-        }
-    }
-
     public function testShowLogFile()
     {
-        $this->log(shell_exec('tail -50 ' . escapeshellarg(self::FILENAME)));
+        $file = APPLICATION_PATH . '/../../log/php_errors.log';
+        $this->assert->disc->file
+            ->exists($file)
+            ->tail($file, 50);
     }
 
 }
