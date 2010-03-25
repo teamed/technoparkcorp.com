0ee118dcd9b0792d2054ff4d528f1059f0a33335
Yegor Bugayenko <yegor@tpc2.com>
2010-03-25 08:30:58 +0000

    some fixes in rack test

--- php/src/rack-tests/LogTest.php
+++ php/src/rack-tests/LogTest.php
@@ -8,10 +8,8 @@ class LogTest extends PhpRack_Test
 
     public function testShowLogFile()
     {
-        $file = APPLICATION_PATH . '/../../log/php_errors.log';
         $this->assert->disc->file
-            ->exists($file)
-            ->tail($file, 50);
+            ->tail(APPLICATION_PATH . '/../../log/php_errors.log', 50);
     }
 
 }
