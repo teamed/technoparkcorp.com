7556d9d550aa45d7c4bcf7245e448f8f08c86ecb
Yegor Bugayenko <yegor@tpc2.com>
2010-03-26 16:30:19 +0000

    ajax reloading of rack tests

--- php/src/rack-tests/LogTest.php
+++ php/src/rack-tests/LogTest.php
@@ -8,6 +8,11 @@ class LogTest extends PhpRack_Test
 
     public function testShowLogFile()
     {
+        $this->_setAjaxOptions(
+            array(
+                'reload' => 5, // every 5 seconds, if possible
+            )
+        );
         $this->assert->disc->file
             ->tail(APPLICATION_PATH . '/../../log/php_errors.log', 50);
     }
--- php/src/rack-tests/WebFrontTest.php
+++ php/src/rack-tests/WebFrontTest.php
@@ -8,6 +8,11 @@ class WebFrontTest extends PhpRack_Test
 
     public function testWebFront()
     {
+        $this->_setAjaxOptions(
+            array(
+                'reload' => 5, // every 5 seconds, if possible
+            )
+        );
         $this->assert->network->url
             ->url('http://www.tpc2.com')
             ->regex('TechnoPark');
