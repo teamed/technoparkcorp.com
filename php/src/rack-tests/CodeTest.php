5d4c2e8980eaf7d5785bebb6155bfb2c72b5f9d9
Yegor Bugayenko <yegor@tpc2.com>
2010-03-26 18:00:32 +0000

    autostart disable

--- php/src/rack-tests/CodeTest.php
+++ php/src/rack-tests/CodeTest.php
@@ -8,6 +8,11 @@ class CodeTest extends PhpRack_Test
 
     public function testLint()
     {
+        $this->_setAjaxOptions(
+            array(
+                'autoStart' => false, // don't reload it from start
+            )
+        );
         // show full phpinfo() listing
         $this->assert->php->lint(
             APPLICATION_PATH . '/..',
