c59634fd0592704b2e9044c07196e7ee8ff42216
Yegor Bugayenko <yegor@tpc2.com>
2010-03-28 09:28:24 +0000

    server test intro in rack

---
+++ php/src/rack-tests/ServerTest.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @version $Id: ProcessesTest.php 1469 2010-03-26 17:07:00Z yegor256@yahoo.com $
+ */
+
+class ServerTest extends PhpRack_Test
+{
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->_setAjaxOptions(
+            array(
+                'reload' => 5, // every 5 seconds, if possible
+            )
+        );
+    }
+
+    public function testUptime()
+    {
+        $this->_log('uptime: ' . shell_exec('uptime'));
+    }
+
+    public function testShowProcesses()
+    {
+        $cmd = 'ps -o "%cpu %mem nice user time stat command" -ax | ' .
+        'awk \'NR==1; NR > 1 {print $0 | "sort -k 1 -r"}\' | ' .
+        'grep -v "^ 0.0"';
+        $this->_log('$ ' . $cmd);
+        $this->_log(shell_exec($cmd));
+    }
+
+}   
