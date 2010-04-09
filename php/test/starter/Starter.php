680f97705175397ae89bf89b5fbed5dee04e9489
Yegor Bugayenko <yegor@tpc2.com>
2010-04-09 15:57:22 +0000

    ping

--- php/test/starter/Starter.php
+++ php/test/starter/Starter.php
@@ -8,7 +8,10 @@ class Starter extends FaZend_Test_Starter
 
     protected function _startDatabase() 
     {
+        // bootstrap the database connection
         $this->_bootstrap('db');
+        
+        // remove all tables and views from the DB.
         $this->_dropDatabase();
     }
         
