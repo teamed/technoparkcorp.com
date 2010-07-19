ba28917148f38fa4200023b0b08a956c798b4df5
Yegor Bugayenko <yegor@tpc2.com>
2010-07-19 08:46:22 +0000

    some fixes

--- php/test/injector/Injector.php
+++ php/test/injector/Injector.php
@@ -3,8 +3,6 @@
  * @version $Id$
  */
 
-require_once 'Mocks/artifacts/ProjectRegistry/Project.php';
-
 class Injector extends FaZend_Test_Injector 
 {
     
@@ -17,105 +15,16 @@ class Injector extends FaZend_Test_Injector
         $this->_bootstrap('db');
     }
     
-    protected function _injectSoapClient() 
-    {
-        // this class will catch all calls to fazend
-        require_once 'Shared/Soap/Gateway.php';
-        Shared_Soap_Gateway::setSoapClient(Mocks_Shared_Soap_Client::get());
-        
-        // no cache, since there are NO real SOAP calls to FaZend
-        Shared_Soap_Gateway::setCacheEnabled(false);
-    }
-    
-    protected function _injectSharedMocks()
-    {
-        // clean Shared cache, if necessary
-        // Shared_Cache::getInstance('Shared_SOAP_Gateway')->clean();
-
-        // don't go into real Shared resources
-        Shared_XmlRpc::setXmlRpcClientClass('Mocks_Shared_XmlRpc');
-        Shared_Trac::setTicketClass('Mocks_Shared_Trac_Ticket');
-        Shared_Wiki::setRqdqlProxy('Mocks_Shared_Wiki_RqdqlProxy');
-        Shared_Pan::setSoapClient(Mocks_Shared_Pan_SoapClient::get());
-        
-        // make sure we are getting just small number of test tickets
-        Model_Asset_Defects_Fazend_Trac::setTicketsPerPage(3);
-
-        // instruct them to logg all events
-        Shared_Trac::setVerbose(true);
-        Shared_Wiki::setVerbose(true);
-    }
-        
     protected function _injectMiscellaneous() 
     {
-        // disable file moving after uploading
-        Model_Artifact_Attachments::setLocation(false);
-
         // just to try the translation
         $this->_bootstrap('fz_translate');
         Zend_Registry::get('Zend_Translate')->setLocale(new Zend_Locale('ru'));
     }
     
-    protected function _injectWeakValidation()
-    {
-        require_once 'artifacts/ProjectRegistry/Project/Scope/Deliverables/loaders/Issues.php';
-        DeliverablesLoaders_Issues::setStrictValidation(false);
-    }
-
     protected function _injectSearchProxy() 
     {
         Model_Article::setSearchProxy(new Mocks_Model_Article_SearchProxy());
-    }
-
-    protected function _injectTesterIsLoggedIn() 
-    {
-        // do it after fazend only
-        $this->_bootstrap('fz_orm');
-        $this->_bootstrap('fz_deployer');
-        $this->_bootstrap('fz_session');
-        // in testing environment you do EVERYTHING under this role
-        // in order to avoid conflicts with real documents in
-        // real environment (fazend for example)
-        Model_User::setSession(new FaZend_StdObject());
-        $pms = Mocks_Model_Project::get()->getStakeholdersByRole('PM');
-        $pm = array_shift($pms);
-        Model_User::logIn($pm);
-    }
-
-    protected function _injectTestProject() 
-    {
-        // do it after fazend only
-        $this->_bootstrap('fz_orm');
-        $this->_bootstrap('fz_deployer');
-        $this->_bootstrap('fz_session');
-        // disable any activities with any LIVE projects
-        Model_Project::setWeAreManaging(false);
-
-        // add it to registry
-        theProjectRegistry::addExtra(Mocks_Model_Project::NAME, new Mocks_theProject());
-    }
-
-    protected function _injectAccessRights() 
-    {
-        // do it after fazend only
-        $this->_bootstrap('fz_orm');
-        $this->_bootstrap('fz_deployer');
-        $this->_bootstrap('fz_session');
-        $this->_bootstrap('fz_view');
-        $this->_bootstrap('fz_routes');
-        if (Model_User::isLoggedIn()) {
-            // initialize ACL
-            $acl = Model_Pages::getInstance()->getAcl();
-        
-            // add this role to ACL
-            $email = Model_User::me()->email;
-            if (!$acl->hasRole($email)) {
-                $acl->addRole($email);
-            }
-        
-            // give access to everything for the testing user
-            $acl->allow($email);
-        }
     }
 
 }
