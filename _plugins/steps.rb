4627a07d66fad6f2b4b0767437dceaa95c7d93b6
Yegor Bugayenko <yegor@tpc2.com>
2014-08-04 20:47:53 +0200

    steps work

--- _plugins/steps.rb
+++ _plugins/steps.rb
@@ -3,12 +3,23 @@ module Tpc
     class Steps < Liquid::Tag
       def render(context)
         page = context['page']
-        if page['next'].nil?
+        if page['next_step'].nil?
           nxt = 'process/scope'
         else
-          nxt = page['next'].strip
+          nxt = page['next_step'].strip
         end
-        li(find(nxt, context))
+        ul(nxt, context, 0)
+      end
+
+      def ul(name, context, total)
+        return '' unless total < 4
+        post = find(name, context)
+        html = li(post)
+        nxt = post['next_step']
+        if nxt
+          html += ul(nxt, context, total + 1)
+        end
+        html
       end
 
       def find(name, context)
