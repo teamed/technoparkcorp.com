2ed79fa72fe433ca648ffc54ebd54f7f1902155c
Yegor Bugayenko <yegor@tpc2.com>
2014-08-05 13:28:22 +0200

    latex works

--- _plugins/tikz.rb
+++ _plugins/tikz.rb
@@ -7,38 +7,35 @@ module Tpc
       def render(context)
         site = context.registers[:site]
         name = Digest::MD5.hexdigest(super)
-        if !File.exists?(File.join(site.dest, "tikz/#{name}.png"))
-          temp = File.join(site.dest, ".tikz-temp")
-          FileUtils.mkdir_p(temp)
-          File.open(File.join(temp, "#{name}.tex"), 'w') { |f|
-            f.write(super)
-          }
+        if !File.exists?(File.join(site.source, "tikz/#{name}.png"))
+          tex = File.join(site.source, ".tikz-temp/tikz.tex")
+          FileUtils.mkdir_p(File.dirname(tex))
+          File.open(tex, 'w') { |f| f.write(super) }
           system(
             [
-              "cd #{site.dest}",
+              "cd #{site.source}",
               "mkdir -p tikz",
               "cd .tikz-temp",
-              "cat #{site.source}/_latex/header.tex > doc.tex",
-              "cat #{name}.tex >> doc.tex",
+              "cat ../_latex/header.tex > doc.tex",
+              "cat tikz.tex >> doc.tex",
               "echo '\\end{document}' >> doc.tex",
-              "latex -halt-on-error -interaction=nonstopmode doc.tex",
+              "latex -halt-on-error -interaction=nonstopmode doc.tex >/dev/null",
               "dvips -o doc.ps doc.dvi",
               [
                 "echo quit",
                 "gs -q -dNOPAUSE -sDEVICE=ppmraw -sOutputFile=- -r300 doc.ps",
                 "pnmalias -bgcolor rgb:ff/ff/ff -falias -fgcolor rgb:00/00/00 -weight 0.6",
                 "pnmcrop -white",
-                "pnmscale 0.5",
                 "pnmtopng -interlace > doc.png"
               ].join(' | '),
               "mv doc.png ../tikz/#{name}.png"
             ].join(' && ')
           )
-          site.static_files << Jekyll::StaticFile.new(
-            site, site.source, 'tikz', "#{name}.png"
-          )
         end
-        "<p><img src='/tikz/#{name}.png' alt='tikz'/></p>"
+        site.static_files << Jekyll::StaticFile.new(
+          site, site.source, 'tikz', "#{name}.png"
+        )
+        "<p><img src='/tikz/#{name}.png' alt='tikz' class='tikz'/></p>"
       end
     end
   end
