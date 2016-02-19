daff743118def85844c2ed62218570a0caffa93a
Yegor Bugayenko <yegor@tpc2.com>
2016-02-18 20:46:39 -0800

    better tikz processing

--- _plugins/tikz.rb
+++ _plugins/tikz.rb
@@ -11,30 +11,26 @@ module Tpc
           tex = File.join(site.source, ".tikz-temp/tikz.tex")
           FileUtils.mkdir_p(File.dirname(tex))
           File.open(tex, 'w') { |f| f.write(super) }
-          system(
-            [
-              "cd #{site.source}",
-              "mkdir -p tikz",
-              "cd .tikz-temp",
-              "cat ../_latex/header.tex > doc.tex",
-              "cat tikz.tex >> doc.tex",
-              "echo '\\end{document}' >> doc.tex",
-              "latex -halt-on-error -interaction=nonstopmode doc.tex >/dev/null",
-              "dvips -o doc.ps doc.dvi",
-              [
-                "echo quit",
-                "gs -q -dNOPAUSE -sDEVICE=ppmraw -sOutputFile=- -r300 doc.ps",
-                "pnmalias -bgcolor rgb:ff/ff/ff -falias -fgcolor rgb:00/00/00 -weight 0.6",
-                "pnmcrop -white",
-                "pnmtopng -interlace > doc.png"
-              ].join(' | '),
-              "mv doc.png ../tikz/#{name}.png",
-              "cd .."
-            ].join(' && ')
-          )
-          if $?.exitstatus != 0
-            raise 'failed to compile Tikz'
-          end
+          puts %x[
+            set -e
+            cd #{site.source}
+            mkdir -p tikz
+            mkdir -p .tikz-temp
+            cd .tikz-temp
+            cat ../_latex/header.tex > doc.tex
+            cat tikz.tex >> doc.tex
+            echo '\\end{document}' >> doc.tex
+            latex -halt-on-error -interaction=nonstopmode doc.tex >/dev/null
+            dvips -o doc.ps doc.dvi
+            echo quit | \\
+              gs -q -dNOPAUSE -sDEVICE=ppmraw -sOutputFile=- -r300 doc.ps | \\
+              pnmalias -bgcolor rgb:ff/ff/ff -falias -fgcolor rgb:00/00/00 -weight 0.6 | \\
+              pnmcrop -white | \\
+              pnmtopng -interlace > doc.png
+            mv doc.png ../tikz/#{name}.png
+            cd ..
+          ]
+          raise 'failed to compile Tikz' if !$?.exitstatus
         end
         site.static_files << Jekyll::StaticFile.new(
           site, site.source, 'tikz', "#{name}.png"
